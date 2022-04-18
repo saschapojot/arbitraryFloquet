5a9fd29dd10b22ab53cce7b810af9a5ee7ea4730
saschapojot <1992xi.liu@gmail.com>
2022-04-19 05:11:08 +0800

    to csv added

--- pltOneMbcx.py
+++ pltOneMbcx.py
@@ -4,18 +4,23 @@ from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import expm
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
+from pathlib import Path
+import shutil
+import pandas as pd
+import os
 # from scipy.linalg import expm
-import seaborn as sns
+
 J1 = 0.5 * np.pi
 J3 = 0.2 * np.pi
-J2 = 0.5 * np.pi
+J2Coef=0.5
+J2 = J2Coef * np.pi
 M = 1
 sigma3 = np.array([[1, 0], [0, -1]])
 sigma2 = np.array([[0, -1j], [1j, 0]])
 sigma1 = np.array([[0, 1], [1, 0]])
 
-N1 = 200
-N2 = 200
+N1 = 40
+N2 =40
 dk = 2 * np.pi / N2
 
 # spatial part of H1
@@ -147,13 +152,61 @@ for itemTmp in retAll:
 
 
 #plot 1 figure
-bTmp,phaseTmp,vecTmp=j2indLeftPhaseVec[0]
+# bTmp,phaseTmp,vecTmp=j2indLeftPhaseVec[1]
+#
+# outMat=np.zeros((N1,N2),dtype=float)
+# for n1 in range(0,N1):
+#     for n2 in range(0,N2):
+#         outMat[n1,n2]=1/N2*(np.abs(vecTmp[2*n1])**2+np.abs(vecTmp[2*n1+1])**2)
+#
+#
+# ax=sns.heatmap(outMat)
+# plt.savefig("hm.png")
+outDirLeft="./leftLinearJ2"+str(J2Coef)+"N2"+str(N2)+"/"
+outDirRight="./rightLinearJ2"+str(J2Coef)+"N2"+str(N2)+"/"
+if os.path.exists(outDirLeft) and os.path.isdir(outDirLeft):
+    shutil.rmtree(outDirLeft)
+if os.path.exists(outDirRight) and os.path.isdir(outDirRight):
+    shutil.rmtree(outDirRight)
+Path(outDirLeft).mkdir(parents=True,exist_ok=True)
+Path(outDirRight).mkdir(parents=True,exist_ok=True)
+#plt left
+outTableLeft=[]
+for j in range(0,len(j2indLeftPhaseVec)):
+    b,phaseTmp,vecLeftTmp=j2indLeftPhaseVec[j]
+    oneRowTmp=[N2,b,phaseTmp]
+    oneRowTmp.extend(vecLeftTmp)
+    outTableLeft.append(oneRowTmp)
+    outMat=np.zeros((N1,N2),dtype=float)
+    for n1 in range(0,N1):
+        for n2 in range(0,N2):
+            outMat[n1,n2]=1/N2*(np.abs(vecLeftTmp[2*n1])**2+np.abs(vecLeftTmp[2*n1+1])**2)
 
-outMat=np.zeros((N1,N2),dtype=float)
-for n1 in range(0,N1):
-    for n2 in range(0,N2):
-        outMat[n1,n2]=1/N2*(np.abs(vecTmp[2*n1])**2+np.abs(vecTmp[2*n1+1])**2)
+    plt.figure()
+    im=plt.imshow(outMat,cmap=plt.cm.RdBu,interpolation="bilinear")
+    plt.colorbar(im)
+    plt.savefig(outDirLeft+"leftb"+str(b)+"j"+str(j)+".png")
+    plt.close()
 
+leftDtfrm=pd.DataFrame(data=outTableLeft)
+leftDtfrm.to_csv(outDirLeft+"leftVecsJ2"+str(J2Coef)+".csv",index=False,header=False)
 
-ax=sns.heatmap(outMat)
-plt.savefig("hm.png")
+#plt right
+outTableRight=[]
+for j in range(0,len(j2indRightPhaseVec)):
+    b,phaseTmp,vecRightTmp=j2indRightPhaseVec[j]
+    oneRowTmp=[N2,b,phaseTmp]
+    oneRowTmp.extend(vecRightTmp)
+    outTableRight.append(oneRowTmp)
+    outMat=np.zeros((N1,N2),dtype=float)
+    for n1 in range(0,N1):
+        for n2 in range(0,N2):
+            outMat[n1,n2]=1/N2*(np.abs(vecRightTmp[2*n1])**2+np.abs(vecRightTmp[2*n1+1])**2)
+    plt.figure()
+    im=plt.imshow(outMat,cmap=plt.cm.RdBu,interpolation="bilinear")
+    plt.colorbar(im)
+    plt.savefig(outDirRight+"rightb"+str(b)+"j"+str(j)+".png")
+    plt.close()
+
+rightDtfrm=pd.DataFrame(data=outTableRight)
+rightDtfrm.to_csv(outDirRight+"rightVecsJ2"+str(J2Coef)+".csv",index=False,header=False)
