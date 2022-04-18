74c800772d53b6e7d9169b3083da964913b9e62b
saschapojot <1992xi.liu@gmail.com>
2022-04-18 18:04:49 +0800

    first commit

---
+++ conductance.py
@@ -0,0 +1,121 @@
+import numpy as np
+from datetime import datetime
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import expm
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+
+tStart=datetime.now()
+J1 = 0.5 * np.pi
+J3 = 0.2 * np.pi
+rangeJ2=range(0,12)
+J2ValsAll=[0.5*j*np.pi for j in rangeJ2]
+
+M = 1
+sigma0=np.array([[1,0],[0,1]],dtype=complex)
+sigma3 = np.array([[1, 0], [0, -1]],dtype=complex)
+sigma2 = np.array([[0, -1j], [1j, 0]],dtype=complex)
+sigma1 = np.array([[0, 1], [1, 0]],dtype=complex)
+
+Nx = 70
+Ny = 70
+
+#spatial part of H1
+S1=np.zeros((Nx*Ny,Nx*Ny),dtype=complex)
+for ny in range(0,Ny):
+    for nx in range(0,Nx-1):
+        S1[nx*Ny+ny,(nx+1)*Ny+ny]=1
+        S1[(nx+1)*Ny+ny,nx*Ny+ny]=-1
+
+H1=np.kron(3*J1/(2*1j)*S1,sigma1)
+H1Sparse=csc_matrix(H1)
+U1=expm(-1j*1/3*H1Sparse)
+U1Dense=U1.toarray()
+#spatial part of H2
+S2=np.zeros((Nx*Ny,Nx*Ny),dtype=complex)
+for nx in range(0,Nx):
+    for ny in range(0,Ny-1):
+        S2[nx*Ny+ny,nx*Ny+ny+1]=1
+        S2[nx*Ny+ny+1,nx*Ny+ny]=-1
+
+def H2MatSparse(j):
+    J2Tmp=J2ValsAll[j]
+    return csc_matrix(np.kron(3*J2Tmp/(2*1j)*S2,sigma2))
+def U2DenseMat(j):
+    U2Tmp=expm(-1j*1/3*H2MatSparse(j))
+    return U2Tmp.toarray()
+
+#spatial part of H3
+
+S3=np.zeros((Nx*Ny,Nx*Ny),dtype=complex)
+#part 1
+for nx in range(0,Nx):
+    for ny in range(0,Ny):
+        S3[nx*Ny+ny,nx*Ny+ny]=2*M
+
+#part 2
+for ny in range(0,Ny):
+    for nx in range(0,Nx-1):
+        S3[nx*Ny+ny,(nx+1)*Ny+ny]=1
+        S3[(nx+1)*Ny+ny,nx*Ny+ny]=1
+#part 3
+for nx in range(0,Nx):
+    for ny in range(0,Ny-1):
+        S3[nx*Ny+ny,nx*Ny+ny+1]=1
+        S3[nx*Ny+ny+1,nx*Ny+ny]=1
+
+H3=np.kron(3*J3/2*S3,sigma3)
+H3Sparse=csc_matrix(H3)
+U3=expm(-1j*1/3*H3Sparse)
+U3Dense=U3.toarray()
+
+def UMat(j):
+    UTmp=U3Dense@U2DenseMat(j)@U1Dense
+    return UTmp
+
+EValsAll=[0.065*np.pi,0.935*np.pi]
+
+pSp=np.zeros((Nx*Ny,Nx*Ny),dtype=complex)
+for n in range(0,Ny):
+    pSp[n,n]=1
+for n in range(1,Ny+1):
+    pSp[-n,-n]=1
+
+P=np.kron(pSp,sigma0)
+
+PTP=P.T@P
+
+def G(EIndj):
+    EInd,j=EIndj
+    E=EValsAll[EInd]
+    U=UMat(j)
+    nrow,ncol=U.shape
+    id=np.eye(nrow,dtype=complex)
+    S=P@np.linalg.inv(id-np.exp(1j*E)*U@(id-PTP))*np.exp(1j*E)@U@P.T
+    tE=S[0:(2*Ny),(-2*Ny):]
+    return [EInd,j,np.real(np.trace(tE.T.conj()@tE))]
+
+threadNum=24
+
+inDataAll=[[EInd, j] for EInd in [0,1] for j in rangeJ2]
+
+pool0=Pool(threadNum)
+retAll=pool0.map(G,inDataAll)
+
+tEnd=datetime.now()
+print("computation time: ",tEnd-tStart)
+
+GE0=[]
+GEpi=[]
+for itemTmp in retAll:
+    EInd,j,GTmp=itemTmp
+    if EInd==0:
+        GE0.append(GTmp)
+    else:
+        GEpi.append(GTmp)
+
+plt.figure()
+J2Plt=[elem/np.pi for elem in J2ValsAll]
+plt.plot(J2Plt,GE0,color="black")
+plt.plot(J2Plt,GEpi,color="red")
+plt.savefig("tmp.png")
---
+++ spectrum.py
@@ -0,0 +1,183 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.linalg import expm
+from multiprocessing import Pool
+from datetime import datetime
+
+J1=0.5*np.pi
+J3=0.2*np.pi
+M=1
+sigma3=np.array([[1,0],[0,-1]])
+sigma2=np.array([[0,-1j],[1j,0]])
+sigma1=np.array([[0,1],[1,0]])
+
+N=100
+dk=2*np.pi/N
+#k ind from 0 to N-1
+
+
+
+J2Min=0.1*np.pi
+J2Max=9.5*np.pi
+dJ2=0.1*np.pi
+maxIndJ2=int((J2Max-J2Min)/dJ2)+2
+
+J2ValsAll=[J2Min+n*dJ2 for n in range(0,maxIndJ2)]
+
+
+def F3(ab):
+    a,b=ab
+    ka=a*dk
+    kb=b*dk
+    return [a,b,expm(-1j*J3*(M+np.cos(ka)+np.cos(kb))*sigma3)]
+
+def F2(bJ2Ind):
+    b,J2Ind=bJ2Ind
+    J2=J2ValsAll[J2Ind]
+    kb=b*dk
+    return [b,J2Ind,expm(-1j*J2*np.sin(kb)*sigma2)]
+
+def F1(a):
+    ka=a*dk
+    return [a,expm(-1j*J1*np.sin(ka)*sigma1)]
+
+
+
+
+F3Tensor=np.zeros((N,N,2,2),dtype=complex)
+
+F2Tensor=np.zeros((N,maxIndJ2,2,2),dtype=complex)
+
+F1Tensor=np.zeros((N,2,2),dtype=complex)
+
+F3InData=[[a,b] for a in range(0,N) for b in range(0,N)]
+F2InData=[[b,J2Ind] for b in range(0,N) for J2Ind in range(0,maxIndJ2)]
+F1InData=[a for a in range(0,N)]
+
+threaNum=24
+tFStart=datetime.now()
+pool0=Pool(threaNum)
+ret3=pool0.map(F3,F3InData)
+
+
+pool1=Pool(threaNum)
+ret2=pool1.map(F2,F2InData)
+
+pool2=Pool(threaNum)
+ret1=pool2.map(F1,F1InData)
+
+
+tFEnd=datetime.now()
+print("F time : ",tFEnd-tFStart)
+
+#populate F3Tensor
+for itemTmp in ret3:
+    a,b,UTmp=itemTmp
+    F3Tensor[a,b,:,:]=UTmp
+
+#populate F2Tensor
+for itemTmp in ret2:
+    b,J2Ind,UTmp=itemTmp
+    F2Tensor[b,J2Ind,:,:]=UTmp
+
+#populate F1Tensor
+for itemTmp in ret1:
+    a,UTmp=itemTmp
+    F1Tensor[a,:,:]=UTmp
+
+
+
+def UBarEigValAndVecs(abJ2Ind):
+    a,b,J2Ind=abJ2Ind
+    F3Tmp=F3Tensor[a,b,:,:]
+    F2Tmp=F2Tensor[b,J2Ind,:,:]
+    F1Tmp=F1Tensor[a,:,:]
+    UTmp=F3Tmp@F2Tmp@F1Tmp
+    eigs, vecs=np.linalg.eig(UTmp)
+    phases=np.angle(eigs)
+    inds=np.argsort(phases)
+    phasesSorted=[phases[ind] for ind in inds]
+    vecsSorted=[vecs[:,ind] for ind in inds]
+    return [a,b,J2Ind,phasesSorted,vecsSorted]
+
+
+abJ2IndAll=[[a,b,J2Ind] for a in range(0,N) for b in range(0,N) for J2Ind in range(0,maxIndJ2)]
+
+tUBarStart=datetime.now()
+pool3=Pool(threaNum)
+
+retUBarEigAll=pool3.map(UBarEigValAndVecs,abJ2IndAll)
+tUBarEnd=datetime.now()
+print("UBar time: ",tUBarEnd-tUBarStart)
+
+eigVecsTensor=np.zeros((maxIndJ2,N,N,2,2),dtype=complex)
+tInitStart=datetime.now()
+for itemTmp in retUBarEigAll:
+    a,b,J2Ind,phases,vecs=itemTmp
+    eigVecsTensor[J2Ind,a,b,:,0]=vecs[0]
+    eigVecsTensor[J2Ind,a,b,:,1]=vecs[1]
+
+tInitEnd=datetime.now()
+print("init time: ",tInitEnd-tInitStart)
+
+def cherNum(J2IndbandNum):
+    J2Ind,bandNum=J2IndbandNum
+
+    chTmp=0
+    for a in range(0,N):
+        for b in range(0,N):
+            vecab=eigVecsTensor[J2Ind,a,b,:,bandNum]
+            vecap1b=eigVecsTensor[J2Ind,(a+1)%N,b,:,bandNum]
+            vecap1bp1=eigVecsTensor[J2Ind,(a+1)%N,(b+1)%N,:,bandNum]
+            vecabp1=eigVecsTensor[J2Ind,a,(b+1)%N,:,bandNum]
+            chTmp+=-np.angle(
+                np.vdot(vecab,vecap1b)
+                *np.vdot(vecap1b,vecap1bp1)
+                *np.vdot(vecap1bp1,vecabp1)
+                *np.vdot(vecabp1,vecab)
+            )
+    return [J2Ind,bandNum,chTmp/(2*np.pi)]
+
+
+inChNumData=[[J2Ind,bandNum] for J2Ind in range(0,maxIndJ2) for bandNum in [0,1]]
+
+pool4=Pool(threaNum)
+
+
+t4Start=datetime.now()
+ret4=pool4.map(cherNum,inChNumData)
+t4End=datetime.now()
+
+print("Chern number time: ",t4End-t4Start)
+
+#data serialization
+
+J2Band0=[]
+J2Band1=[]
+chNumBand0=[]
+chNumBand1=[]
+for itemTmp in ret4:
+    J2Ind, bandNum,chTmp=itemTmp
+    if bandNum==0:
+        J2Band0.append(J2ValsAll[J2Ind]/np.pi)
+        chNumBand0.append(chTmp)
+    else:
+        J2Band1.append(J2ValsAll[J2Ind]/np.pi)
+        chNumBand1.append(chTmp)
+
+
+plt.figure()
+plt.plot(J2Band0,chNumBand0,color="blue",label="$C_{0}$",linestyle="-")
+
+plt.plot(J2Band1,chNumBand1,color="red",label="$C_{1}$",linestyle="--")
+
+xTickStart=int(J2ValsAll[0]/np.pi)
+xTickEnd=int(J2ValsAll[-1]/np.pi)+1
+plt.xticks(range(xTickStart,xTickEnd))
+plt.xlabel("$J_{2}/\pi$")
+plt.ylabel("Chern number")
+plt.yticks([-1,0,1])
+
+
+plt.legend()
+plt.savefig("spectrum.png")
